4706310f1090ff0e2ea8547830d9ab28a8c4fd81
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-07 15:05:02 -0500

    chore: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "$@"
 mkdir ~/.aws
 echo "[profile $INPUT_AWS_PROFILE]\noutput = json" >> ~/.aws/config
 mfa_response=`echo $INPUT_OKTA_MFA_SEED | mintotp`
-tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method $INPUT_OKTA_MFA_METHOD --mfa-response $mfa_response >> /dev/null
+tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response $mfa_response >> /dev/null
