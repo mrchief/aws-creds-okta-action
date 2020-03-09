7115cb96480b2b5ed288a7719ef4cd35b8aa7adb
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-09 10:41:37 -0400

    fix: Write to mapped github home instead of runner home

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir ~/.aws
-echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> ~/.aws/config
-tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` >> /dev/null
+mkdir /github/home/.aws
+echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> /github/home/.aws/config
+tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o /github/home/.aws/credentials >> /dev/null
