d2ea4233e4af113a736df3f8c2d16ba906dcb42a
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-09 10:51:57 -0400

    fix: Another pass at writing creds to home

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir /github/home/.aws
-echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> /github/home/.aws/config
-tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o /github/home/.aws/credentials >> /dev/null
+CREDS_DIR="$HOME/.aws"
+mkdir $CREDS_DIR
+echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> $CREDS_DIR/config
+tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o $CREDS_DIR/credentials >> /dev/null
