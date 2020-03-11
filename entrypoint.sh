ed8289b484e54aea1ef1f054c2e8b001c5424a19
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-11 00:45:12 -0400

    chore: Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 printenv
 
-CREDS_DIR="~/.aws"
+CREDS_DIR="${HOME}/.aws"
 mkdir "${CREDS_DIR}"
 echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> "${CREDS_DIR}/config"
 tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o "${CREDS_DIR}/credentials" >> /dev/null
