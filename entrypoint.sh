dc2ac5c1eae86e4f9b23decb0b0a896e7f2857d5
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-11 00:32:43 -0400

    chore: Another crutch

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_
 
 # since we can't write to runner's home, export these vars so that AWS CLI can find the files
 # https://github.community/t5/GitHub-Actions/Docker-action-can-t-create-folder-in-runner-s-home-directory/m-p/49612
-echo "::set-env name=AWS_CONFIG_FILE::$RUNNER_WORKSPACE/.aws/config"
-echo "::set-env name=AWS_SHARED_CREDENTIALS_FILE::$RUNNER_WORKSPACE/.aws/credentials"
+echo "::set-env name=AWS_CONFIG_FILE::$INPUT_WORKSPACE/.aws/config"
+echo "::set-env name=AWS_SHARED_CREDENTIALS_FILE::$INPUT_WORKSPACE/.aws/credentials"
