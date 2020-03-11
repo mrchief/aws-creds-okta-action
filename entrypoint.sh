690c662495efa0b7c51320c25128e20c1aff7d21
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-03-11 00:50:34 -0400

    chore: GH Actions fuckery

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 printenv
 
 CREDS_DIR="${HOME}/.aws"
-mkdir "${CREDS_DIR}"
+mkdir -p "${CREDS_DIR}"
 echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> "${CREDS_DIR}/config"
-tokendito -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o "${CREDS_DIR}/credentials" >> /dev/null
+tokendito -c "${CREDS_DIR}/config" -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o "${CREDS_DIR}/credentials" >> /dev/null
 
 mv "${CREDS_DIR}" "${GITHUB_WORKSPACE}"
 # since we can't write to runner's home, export these vars so that AWS CLI can find the files
