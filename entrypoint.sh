81c0aebaddb92982244fcd7227b999eb3f1fa4f2
Hrusikesh Panda <mrchief_2000@yahoo.com>
2020-03-11 18:56:25 -0400

    fix: Set defaults properly with set -u

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ set -o pipefail
 
 CREDS_DIR="${HOME}/.aws"
 mkdir -p "${CREDS_DIR}"
-echo "[profile ${INPUT_AWS_PROFILE:-default}]\noutput = json" >> "${CREDS_DIR}/config"
-tokendito --config-file "${CREDS_DIR}/config" -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:-token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o "${CREDS_DIR}/credentials" >> /dev/null
+echo "[profile ${INPUT_AWS_PROFILE:=default}]\noutput = json" >> "${CREDS_DIR}/config"
+tokendito --config-file "${CREDS_DIR}/config" -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:=token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` -o "${CREDS_DIR}/credentials" >> /dev/null
 
 # mv "${CREDS_DIR}" "${GITHUB_WORKSPACE}"
 # # since we can't write to runner's home, export these vars so that AWS CLI can find the files
