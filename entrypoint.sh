0b65d55628f346e14af50ba134882147d5dc6c05
Hrusikesh Panda <mrchief_2000@yahoo.com>
2020-03-12 21:02:37 -0400

    chore: Cleanup dead code

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,6 @@ mkdir -p "${awsDir}"
 echo -e "[profile default]\noutput = json" >> "$config"
 tokendito --aws-profile default -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD --mfa-method ${INPUT_OKTA_MFA_METHOD:=token:software:totp} --mfa-response `echo $INPUT_OKTA_MFA_SEED | mintotp` >> /dev/null
 
-# mv "${CREDS_DIR}" "${GITHUB_WORKSPACE}"
-# # since we can't write to runner's home, export these vars so that AWS CLI can find the files
-# # https://github.community/t5/GitHub-Actions/Docker-action-can-t-create-folder-in-runner-s-home-directory/m-p/49612
-# echo "::set-env name=AWS_CONFIG_FILE::${INPUT_WORKSPACE}/.aws/config"
-# echo "::set-env name=AWS_SHARED_CREDENTIALS_FILE::${INPUT_WORKSPACE}/.aws/credentials"
-
 # Read credentials
 section=
 while read -r line; do
