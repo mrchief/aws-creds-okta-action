3c49b513e5ae2a9db362e7688446683fbb08c4d3
Cesar Manrique <CesarManriqueH@users.noreply.github.com>
2020-06-19 17:54:58 -0500

    fix: Retry tokendito only on errors due to used totp code (#9)
    Co-authored-by: Hrusikesh Panda <mrchief@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,20 +13,28 @@ echo -e "[profile default]\noutput = json" >> "$config"
 # Attempt to get aws credentials via tokendito
 max_attempts=10
 totp_time=30
+totp_error='Each code can only be used once. Please wait for a new code and try again.'
 for ((attempts = 1; attempts <= $max_attempts ; attempts++)); do
-    tokendito --aws-profile default -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN \
+    echo "Requesting AWS credentials via Tokendito."
+    t_error=$(tokendito --aws-profile default -ou $INPUT_OKTA_APP_URL -R $INPUT_AWS_ROLE_ARN \
         --username $INPUT_OKTA_USERNAME --password $INPUT_OKTA_PASSWORD \
         --mfa-method ${INPUT_OKTA_MFA_METHOD:=token:software:totp} \
-        --mfa-response $(echo $INPUT_OKTA_MFA_SEED | mintotp ${totp_time}) >> /dev/null
+        --mfa-response $(echo $INPUT_OKTA_MFA_SEED | mintotp ${totp_time}) \
+        2>&1 1>/dev/null)
 
     if [[ $? == 0 ]]; then
         echo "Succeeded getting credentials in attempt #${attempts}."
         break
     fi
 
-    echo "An error occurred while getting credentials. Attempt #${attempts}."
-    echo "Waiting ${totp_time} seconds before retrying..."
-    sleep ${totp_time}
+    if [[ $t_error == *$totp_error* ]]; then
+        echo "Attempt #${attempts} => ERROR: ${totp_error}"
+        echo -e "Waiting ${totp_time} seconds before retrying...\n"
+        sleep $totp_time
+    else
+        echo $t_error
+        exit 1
+    fi
 done
 
 if [[ $attempts == $((max_attempts+1)) ]]; then
